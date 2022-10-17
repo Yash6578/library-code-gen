fc88d5206b3211c794b60a0c4427578d8752cba4
yash6578 <yashchawla6578@gmail.com>
2022-10-17 19:47:25 +0530

    Initial commit

---
+++ src/main/java/com/accenture/librarycodegen/LibraryCodeGenApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.librarycodegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCodeGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCodeGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/librarycodegen/LibraryCodeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.librarycodegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCodeGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
